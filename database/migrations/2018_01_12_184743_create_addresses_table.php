666b35a3a6635304e03971fffe4324e71237cf50
Filip Vucinic <filigor94@gmail.com>
2018-01-12 19:58:10 +0100

    Created migrations
    Festival and user migrations created.

---
+++ database/migrations/2018_01_12_184743_create_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('country');
+            $table->string('city');
+            $table->string('address');
+            $table->string('number');
+            $table->string('map_ltd');
+            $table->string('map_lon');
+            $table->integer('id_festival')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
